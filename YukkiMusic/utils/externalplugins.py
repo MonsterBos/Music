ee66c76c70479fdaff6b828b5655ba77cd73718f
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-14 12:44:51 +0530

    Update externalplugins.py

--- YukkiMusic/utils/externalplugins.py
+++ YukkiMusic/utils/externalplugins.py
@@ -1,20 +1,26 @@
 import os
 import shutil
-import subprocess
-import logging
+import git
 
+def copy_plugins(source, destination):
+    # Iterate over all files and folders in the source directory
+    for item in os.listdir(source):
+        source_path = os.path.join(source, item)
+        destination_path = os.path.join(destination, item)
+        # If it's a file, copy it to the destination folder
+        if os.path.isfile(source_path):
+            shutil.copy2(source_path, destination_path)
+        # If it's a folder, recursively copy its contents
+        elif os.path.isdir(source_path):
+            shutil.copytree(source_path, destination_path)
 
-def load_external_plugin(repo_url="https://github.com/Vivekkumar-IN/External-Plugins"):
-    destination_directory = "YukkiMusic/plugins/external"
-
-    try:
-        subprocess.run(["git", "clone", repo_url])
-        repo_name = repo_url.split("/")[-1].split(".")[0]
-        os.makedirs(destination_directory, exist_ok=True)
-        for filename in os.listdir(repo_name):
-            src = os.path.join(repo_name, filename)
-            dest = os.path.join(destination_directory, filename)
-            shutil.move(src, dest)
-        shutil.rmtree(repo_name)
-    except Exception as e:
-        logging.exception(e)
+def load_external_plugin(repo_url="https://github.com/Vivekkumar-IN/External-Plugins", main_repo_path="YukkiMusic/plugins/tools"):
+    # Clone the repository to a temporary folder
+    temp_repo_path = "/tmp/temp_repo"
+    git.Repo.clone_from(repo_url, temp_repo_path)
+    # Navigate to the plugins folder in the cloned repository
+    source_plugins_path = os.path.join(temp_repo_path, "plugins")
+    # Copy the contents of the plugins folder to the main repository
+    copy_plugins(source_plugins_path, main_repo_path)
+    # Remove the temporary cloned repository
+    shutil.rmtree(temp_repo_path)
