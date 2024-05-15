38c9dbfc7e12bd68d82c09c23153e67a73609328
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-15 17:16:58 +0530

    Update memorydatabase.py

--- YukkiMusic/utils/database/memorydatabase.py
+++ YukkiMusic/utils/database/memorydatabase.py
@@ -28,6 +28,8 @@ suggdb = mongodb.suggestion
 autoenddb = mongodb.autoend
 coupledb = mongodb.couple
 notesdb = mongodb.notes
+welcomedb = mongodb.welcome
+
 
 # Shifting to memory [ mongo sucks often]
 loop = {}
@@ -144,6 +146,37 @@ async def save_couple(cid: int, date: str, couple: dict, img: str):
         {"$set": {"couple": lovers, "img": img}},
         upsert=True,
     )
+
+
+
+async def get_welcome(chat_id: int) -> (str, str, str):
+    data = await welcomedb.find_one({"chat_id": chat_id})
+    if not data:
+        return "", "", ""
+
+    welcome = data.get("welcome", "")
+    raw_text = data.get("raw_text", "")
+    file_id = data.get("file_id", "")
+
+    return welcome, raw_text, file_id
+
+
+async def set_welcome(chat_id: int, welcome: str, raw_text: str, file_id: str):
+    update_data = {
+        "welcome": welcome,
+        "raw_text": raw_text,
+        "file_id": file_id,
+    }
+
+    return await welcomedb.update_one(
+        {"chat_id": chat_id}, {"$set": update_data}, upsert=True
+    )
+
+
+async def del_welcome(chat_id: int):
+    return await welcomedb.delete_one({"chat_id": chat_id})
+
+
 
 
 # Auto End Stream
