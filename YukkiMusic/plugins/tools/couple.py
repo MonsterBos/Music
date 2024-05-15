bad16883530c0b8cdedfde72eec7091027d65329
VivekKumar-IN <VivekKumar-IN@users.noreply.github.com>
2024-05-15 10:14:09 +0000

    Auto fixes

--- YukkiMusic/plugins/tools/couple.py
+++ YukkiMusic/plugins/tools/couple.py
@@ -1,13 +1,14 @@
-import os 
+import os
 import random
-from datetime import datetime 
+from datetime import datetime
 from telegraph import upload_file
-from PIL import Image , ImageDraw
+from PIL import Image, ImageDraw
 from pyrogram import *
 from pyrogram.types import *
 from pyrogram.enums import *
 from YukkiMusic import app
 from YukkiMusic.utils.database import _get_image, get_couple
+
 
 def dt():
     now = datetime.now()
@@ -26,8 +27,10 @@ def dt_tom():
     )
     return a
 
+
 tomorrow = str(dt_tom())
 today = str(dt()[0])
+
 
 @app.on_message(filters.command("couples"))
 async def ctest(_, message):
@@ -35,65 +38,63 @@ async def ctest(_, message):
     if message.chat.type == ChatType.PRIVATE:
         return await message.reply_text("ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ɪs ᴏɴʟʏ ғᴏʀ ɢʀᴏᴜᴘs.")
     try:
-     #  is_selected = await get_couple(cid, today)
-     #  if not is_selected:
-         msg = await message.reply_text("ɢᴇɴᴇʀᴀᴛɪɴɢ ᴄᴏᴜᴘʟᴇs ɪᴍᴀɢᴇ ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...")
-         #GET LIST OF USERS
-         list_of_users = []
+        #  is_selected = await get_couple(cid, today)
+        #  if not is_selected:
+        msg = await message.reply_text("ɢᴇɴᴇʀᴀᴛɪɴɢ ᴄᴏᴜᴘʟᴇs ɪᴍᴀɢᴇ ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...")
+        # GET LIST OF USERS
+        list_of_users = []
 
-         async for i in app.get_chat_members(message.chat.id, limit=50):
-             if not i.user.is_bot:
-               list_of_users.append(i.user.id)
+        async for i in app.get_chat_members(message.chat.id, limit=50):
+            if not i.user.is_bot:
+                list_of_users.append(i.user.id)
 
-         c1_id = random.choice(list_of_users)
-         c2_id = random.choice(list_of_users)
-         while c1_id == c2_id:
-              c1_id = random.choice(list_of_users)
+        c1_id = random.choice(list_of_users)
+        c2_id = random.choice(list_of_users)
+        while c1_id == c2_id:
+            c1_id = random.choice(list_of_users)
 
+        photo1 = (await app.get_chat(c1_id)).photo
+        photo2 = (await app.get_chat(c2_id)).photo
 
-         photo1 = (await app.get_chat(c1_id)).photo
-         photo2 = (await app.get_chat(c2_id)).photo
+        N1 = (await app.get_users(c1_id)).mention
+        N2 = (await app.get_users(c2_id)).mention
 
-         N1 = (await app.get_users(c1_id)).mention 
-         N2 = (await app.get_users(c2_id)).mention
-
-         try:
+        try:
             p1 = await app.download_media(photo1.big_file_id, file_name="pfp.png")
-         except Exception:
+        except Exception:
             p1 = "Love/upic.png"
-         try:
+        try:
             p2 = await app.download_media(photo2.big_file_id, file_name="pfp1.png")
-         except Exception:
+        except Exception:
             p2 = "Love/upic.png"
 
-         img1 = Image.open(f"{p1}")
-         img2 = Image.open(f"{p2}")
+        img1 = Image.open(f"{p1}")
+        img2 = Image.open(f"{p2}")
 
-         img = Image.open("Love/Couples.png")
+        img = Image.open("Love/Couples.png")
 
-         img1 = img1.resize((1150,1150))
-         img2 = img2.resize((1150,1150))
+        img1 = img1.resize((1150, 1150))
+        img2 = img2.resize((1150, 1150))
 
-         mask = Image.new('L', img1.size, 0)
-         draw = ImageDraw.Draw(mask) 
-         draw.ellipse((0, 0) + img1.size, fill=255)
+        mask = Image.new("L", img1.size, 0)
+        draw = ImageDraw.Draw(mask)
+        draw.ellipse((0, 0) + img1.size, fill=255)
 
-         mask1 = Image.new('L', img2.size, 0)
-         draw = ImageDraw.Draw(mask1) 
-         draw.ellipse((0, 0) + img2.size, fill=255)
+        mask1 = Image.new("L", img2.size, 0)
+        draw = ImageDraw.Draw(mask1)
+        draw.ellipse((0, 0) + img2.size, fill=255)
 
+        img1.putalpha(mask)
+        img2.putalpha(mask1)
 
-         img1.putalpha(mask)
-         img2.putalpha(mask1)
+        draw = ImageDraw.Draw(img)
 
-         draw = ImageDraw.Draw(img)
+        img.paste(img1, (495, 569), img1)
+        img.paste(img2, (2495, 569), img2)
 
-         img.paste(img1, (495, 569), img1)
-         img.paste(img2, (2495, 569), img2)
+        img.save(f"test_{cid}.png")
 
-         img.save(f'test_{cid}.png')
-
-         TXT = f"""
+        TXT = f"""
 **ᴛᴏᴅᴀʏ's sᴇʟᴇᴄᴛᴇᴅ ᴄᴏᴜᴘʟᴇs 💓 :
 
 ➖➖➖➖➖➖➖➖➖➖➖➖
@@ -104,17 +105,17 @@ HAA MERI JAAN
 ɴᴇxᴛ ᴄᴏᴜᴘʟᴇs ᴡɪʟʟ ʙᴇ sᴇʟᴇᴄᴛᴇᴅ ᴏɴ {tomorrow} !!**
 """
 
-         await message.reply_photo(f"test_{cid}.png", caption=TXT)
-         await msg.delete()
-         a = upload_file(f"test_{cid}.png")
-         for x in a:
-           img = "https://graph.org/" + x
-           couple = {"c1_id": c1_id, "c2_id": c2_id}
+        await message.reply_photo(f"test_{cid}.png", caption=TXT)
+        await msg.delete()
+        a = upload_file(f"test_{cid}.png")
+        for x in a:
+            img = "https://graph.org/" + x
+            couple = {"c1_id": c1_id, "c2_id": c2_id}
     except Exception as e:
         print(str(e))
     try:
-      os.remove(f"./downloads/pfp1.png")
-      os.remove(f"./downloads/pfp2.png")
-      os.remove(f"test_{cid}.png")
+        os.remove(f"./downloads/pfp1.png")
+        os.remove(f"./downloads/pfp2.png")
+        os.remove(f"test_{cid}.png")
     except Exception:
-       pass
+        pass
