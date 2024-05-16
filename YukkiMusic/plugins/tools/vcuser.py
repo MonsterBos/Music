0a70606cd48e588e2dba62e10505d8e879d6fc5e
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-16 11:11:27 +0530

    Update vcuser.py

--- YukkiMusic/plugins/tools/vcuser.py
+++ YukkiMusic/plugins/tools/vcuser.py
@@ -2,18 +2,22 @@ from pytgcalls.exceptions import GroupCallNotFound
 from pyrogram import filters
 from YukkiMusic.core.call import Yukki
 from YukkiMusic import app
-
+from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid
 
 @app.on_message(filters.command("vcuser"))
 async def get_vc_users(client, message):
     try:
+        A = await message.replt_text("🔍")
         AB = await Yukki.get_participant(message.chat.id)
     except GroupCallNotFound:
         return await message.reply_text("Assisitant iss not in vc")
     users_info = ""
     for participant in AB:
         user_id = participant.user_id
-        user = await app.get_users(user_id)
-        users_info += f"[{user.first_name}](tg://user?id={user_id})\n"
+        try:
+            user = await app.get_users(user_id)
+            users_info += f"[{user.first_name}](tg://user?id={user_id})\n"
+        except PeerIdInvalid:
+            users_info += f"[ᴜɴᴋɴᴏᴡɴ ᴜsᴇʀ](tg://user?id={user_id})\n"
 
-    await message.reply(users_info)
+    await A.edit(users_info)
