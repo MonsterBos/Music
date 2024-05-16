31b9a51ab588bf077f9aece01943893f9413c7ee
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-16 18:03:50 +0530

    Update profinity.py

--- YukkiMusic/plugins/tools/profinity.py
+++ YukkiMusic/plugins/tools/profinity.py
@@ -47,7 +47,7 @@ async def handle_bad_words(client, message):
             await app.restrict_chat_member(
                 chat_id, user_id, ChatPermissions(), until_date=mute_time
             )
-            await app.send_message(
+            await message.reply_text(
                 message.chat.id,
                 f"{message.from_user.mention} used a bad word: **{censored_text}**, so they are muted for 5 minutes but i have no permission to delete message so give me delete message permission in order to delete bad word automatically",
             )
@@ -63,6 +63,8 @@ async def handle_bad_words(client, message):
             )
             await asyncio.sleep(300)
             await SH.delete()
+        elif not bot.can_restrict_members and not bot.can_delete_messages:
+            await message.reply_text(f"User {message.from_user.mention} has sent **{censored_text}**. the bad word but Please give me permission to restrict members in order to automatically mute users who send bad words for 5 minutes. and delete message permission to delete bad message automatically")
 
     except Exception as e:
         logging.exception(e)
