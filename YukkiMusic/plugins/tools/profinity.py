f3b3a8c65e75bab367dacc8f0d0669bbe5053c15
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-16 17:44:29 +0530

    Update profinity.py

--- YukkiMusic/plugins/tools/profinity.py
+++ YukkiMusic/plugins/tools/profinity.py
@@ -13,11 +13,13 @@ from profanity import profanity
 @app.on_message(filters.text & filters.group)
 async def handle_bad_words(client, message):
     try:
-        bot = (await app.get_chat_member(message.chat.id, app.id)).privileges
-        censored_text = profanity.censor(txt)
-        chat_id = message.chat.id
         txt = message.text
         user_id = message.from_user.id
+        if not profanity.censor(txt):
+            return
+        censored_text = profanity.censor(txt)
+        bot = (await app.get_chat_member(message.chat.id, app.id)).privileges
+        chat_id = message.chat.id
         admins = []
         async for admin in app.get_chat_members(
             message.chat.id, filter=ChatMembersFilter.ADMINISTRATORS
@@ -35,31 +37,29 @@ async def handle_bad_words(client, message):
                 continue
             censored_text += f"[\u2063](tg://user?id={admin.id})"
 
-        if profanity.contains_profanity(txt):
-            if bot.can_delete_messages:
-                await message.delete()
-            else:
-                return await message.reply_text(
+        
+        if bot.can_delete_messages:
+            await message.delete()
+        else:
+            return await message.reply_text(
                     f"User {message.from_user.mention} has sended **{censored_text}** bad word give me delete message permission or ban permission to delete and mute user automatically for 5 minute who send bad word"
                 )
-            if bot.can_restrict_members:
-                mute_time = datetime.datetime.now() + datetime.timedelta(minutes=5)
-                await app.restrict_chat_member(
+        if bot.can_restrict_members:
+            mute_time = datetime.datetime.now() + datetime.timedelta(minutes=5)
+            await app.restrict_chat_member(
                     chat_id, user_id, ChatPermissions(), until_date=mute_time
                 )
-                SH = await app.send_message(
+            SH = await app.send_message(
                     message.chat.id,
                     f"{message.from_user.mention} used a bad word: **{censored_text}**, so they are muted for 5 minutes",
                 )
-                await asyncio.sleep(300)
-                await SH.delete()
-            else:
-                await app.send_message(
+            await asyncio.sleep(300)
+            await SH.delete()
+        else:
+            await app.send_message(
                     message.chat.id,
                     f"{message.from_user.mention} used a bad word: **{censored_text}**. Please give me ban power to mute users who will send bad words for 5 minutes",
                 )
-        else:
-            return
 
     except Exception as e:
         logging.exception(e)
