af53f6825973cc80f94999e50d256ab8967a4b94
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-16 09:09:38 +0530

    Update profinity.py

--- YukkiMusic/plugins/tools/profinity.py
+++ YukkiMusic/plugins/tools/profinity.py
@@ -1,13 +1,1 @@
-"""from pyrogram import Client, filters
-from profanity_check import predict, predict_prob
-from YukkiMusic import app
 
-
-@app.on_message(filters.private)
-async def check_for_profanity(client, message):
-    text = message.text.lower() if message.text else ""
-    if predict_prob([text])[0] > 0.5:
-        await message.reply("Please refrain from using profanity.")
-    else:
-        pass
-"""
