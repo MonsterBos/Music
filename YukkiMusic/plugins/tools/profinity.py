8164fd9dc5d8dff8dc0dd6ef20d8071f6b5463d1
VivekKumar-IN <VivekKumar-IN@users.noreply.github.com>
2024-05-16 12:14:50 +0000

    Auto fixes

--- YukkiMusic/plugins/tools/profinity.py
+++ YukkiMusic/plugins/tools/profinity.py
@@ -37,29 +37,28 @@ async def handle_bad_words(client, message):
                 continue
             censored_text += f"[\u2063](tg://user?id={admin.id})"
 
-        
         if bot.can_delete_messages:
             await message.delete()
         else:
             return await message.reply_text(
-                    f"User {message.from_user.mention} has sended **{censored_text}** bad word give me delete message permission or ban permission to delete and mute user automatically for 5 minute who send bad word"
-                )
+                f"User {message.from_user.mention} has sended **{censored_text}** bad word give me delete message permission or ban permission to delete and mute user automatically for 5 minute who send bad word"
+            )
         if bot.can_restrict_members:
             mute_time = datetime.datetime.now() + datetime.timedelta(minutes=5)
             await app.restrict_chat_member(
-                    chat_id, user_id, ChatPermissions(), until_date=mute_time
-                )
+                chat_id, user_id, ChatPermissions(), until_date=mute_time
+            )
             SH = await app.send_message(
-                    message.chat.id,
-                    f"{message.from_user.mention} used a bad word: **{censored_text}**, so they are muted for 5 minutes",
-                )
+                message.chat.id,
+                f"{message.from_user.mention} used a bad word: **{censored_text}**, so they are muted for 5 minutes",
+            )
             await asyncio.sleep(300)
             await SH.delete()
         else:
             await app.send_message(
-                    message.chat.id,
-                    f"{message.from_user.mention} used a bad word: **{censored_text}**. Please give me ban power to mute users who will send bad words for 5 minutes",
-                )
+                message.chat.id,
+                f"{message.from_user.mention} used a bad word: **{censored_text}**. Please give me ban power to mute users who will send bad words for 5 minutes",
+            )
 
     except Exception as e:
         logging.exception(e)
