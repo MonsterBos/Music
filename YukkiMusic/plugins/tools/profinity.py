7a5a85a00204d4e00d9ee6617d3c1f2d4e43409d
VivekKumar-IN <VivekKumar-IN@users.noreply.github.com>
2024-05-16 03:11:32 +0000

    Auto fixes

--- YukkiMusic/plugins/tools/profinity.py
+++ YukkiMusic/plugins/tools/profinity.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters
 from profanity_check import predict, predict_prob
 from YukkiMusic import app
 
+
 @app.on_message(filters.private)
 async def check_for_profanity(client, message):
     text = message.text.lower() if message.text else ""
