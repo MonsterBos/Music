4dbfefccd2743cdf4b95bd8fc6086d4198eb5819
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-16 15:37:12 +0530

    Update profinity.py

--- YukkiMusic/plugins/tools/profinity.py
+++ YukkiMusic/plugins/tools/profinity.py
@@ -1,11 +1,13 @@
 import logging
+import asyncio
+import datetime
+from pyrogram import filters
+from pyrogram.enums import ChatMembersFilter
+from pyrogram.types import ChatPermissions
+from config import LOG_GROUP_ID
 from YukkiMusic import app
 from YukkiMusic.misc import SUDOERS
 from profanity import profanity
-from pyrogram import filters
-from pyrogram.enums import ChatMembersFilter, ChatMember
-from pyrogram.errors import ChatAdminRequired
-from config import LOG_GROUP_ID
 
 
 @app.on_message(filters.text & filters.group)
@@ -15,44 +17,47 @@ async def handle_bad_words(client, message):
         txt = message.text
         user_id = message.from_user.id
         admins = []
-        async for i in app.get_chat_members(
+        async for admin in app.get_chat_members(
             message.chat.id,
-            filter=ChatMembersFilter.ADMINISTRATORS or ChatMembersFilter.OWNER,
+            filter=ChatMembersFilter.ADMINISTRATORS | ChatMembersFilter.OWNER,
         ):
-            admins.append(i.user.id)
+            admins.append(admin.user)
 
-        if message.from_user.id in SUDOERS or message.from_user.id in admins:
+        if message.from_user.id in SUDOERS or message.from_user.id in [admin.id for admin in admins]:
             return
-        bot = (await app.get_chat_member(message.chat.id, app.id)).privileges
-        if profanity.contains_profanity(txt) == True:
+
+        bot = await app.get_chat_member(message.chat.id, app.id)
+        if bot.is_bot:
+            return
+
+        if profanity.contains_profanity(txt):
             await message.delete()
-            if bot == None:
-                return
-            B = profanity.censor(txt)
-            for admin in i:
-                if admin.user.is_bot or admin.user.is_deleted:
+            censored_text = profanity.censor(txt)
+            mentioned_admins = ""
+            for admin in admins:
+                if admin.is_bot or admin.is_deleted:
                     continue
-                B += f"[\u2063](tg://user?id={admin.user.id})"
+                mentioned_admins += f"[\u2063](tg://user?id={admin.id})"
+
             if bot.can_restrict_members:
                 mute_time = datetime.datetime.now() + datetime.timedelta(minutes=5)
                 await app.restrict_chat_member(
                     chat_id, user_id, ChatPermissions(), until_date=mute_time
                 )
-                Sh = await app.send_message(
+                await app.send_message(
                     message.chat.id,
-                    f"{message.from_user.mention} used a badword :- **{B}** so he is  muted for 5 minutes ",
+                    f"{message.from_user.mention} used a bad word: **{censored_text}**, so they are muted for 5 minutes {mentioned_admins}",
                 )
             else:
-                Sh = await app.send_message(
+                await app.send_message(
                     message.chat.id,
-                    f"{message.from_user.mention} used a badword :- **{B}** Give me ban power so i mute who send bad word for 5 minutes",
+                    f"{message.from_user.mention} used a bad word: **{censored_text}**. Please give me ban power to mute users who send bad words for 5 minutes {mentioned_admins}",
                 )
 
             await asyncio.sleep(300)
-            await Sh.delete()
         else:
             return
 
     except Exception as e:
         logging.exception(e)
-        await app.send_message(LOG_GROUP_ID, f" in profanity module {e} ")
+        await app.send_message(LOG_GROUP_ID, f"Error in profanity module: {e}")
