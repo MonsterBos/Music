1270ffdb162daf29249e6b144245fb15eb19e52f
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-18 13:53:44 +0530

    Update nsfw.py

--- YukkiMusic/plugins/tools/nsfw.py
+++ YukkiMusic/plugins/tools/nsfw.py
@@ -1,83 +1,55 @@
 import logging 
-
 from os import remove
-
 from lexica import Client as lexi
-
 from telegraph import upload_file
-
 from pyrogram import filters
-
 from pyrogram.types import Message
-
 from YukkiMusic import app
-
 from YukkiMusic.misc import SUDOERS
-
 from YukkiMusic.utils.error import capture_err
-
 from config import adminlist
 
-
 def check_nsfw(image_url: str) -> dict:
-
     client = lexi()
-
     response = client.AntiNsfw(image_url)
-
     if response['content']['sfw'] == True:
-
         return False
-
     else:
-
-        return True
-
-        
+        return True       
 
 @app.on_message(
-
     ~filters.service
-
     & ~filters.private
-
     & ~filters.channel
-
     & filters.photo,
-
     group=6
-
 )
 
 @capture_err
-
 async def nsfw(_, message: Message):
+    if message.from_user.id in adminlist or message.from_user.id in SUDOERS:
+        return 
 
-	if message.from_user.id in adminlist or message.from_user.id in SUDOERS:
-
-		return 
-
-	photo = await app.download_media(message.photo.file_id)
+    photo = await app.download_media(message.photo.file_id)
     a = upload_file(media)[0]
     url = "https://telegra.ph" + a
-	try:
+    try:
+        nsfw = check_nsfw(url)
 
-    	nsfw = check_nsfw(url)
+    	   if nsfw == True:
 
-    	if nsfw == True:
+	      	await message.reply_text("Nsfw detected")
 
-	    	await message.reply_text("Nsfw detected")
+    	   elif nsfw == False:
 
-    	elif nsfw == False:
+	       	await message.reply_text("safe file no nsfw detected")
 
-	    	await message.reply_text("safe file no nsfw detected")
+	       remove(photo)
 
-	    remove(photo)
+	   except Exception as e:
 
-	except Exception as e:
+		   remove(photo)
 
-		remove(photo)
-
-		logging.execption(e)
+	   	logging.execption(e)
 
 	
