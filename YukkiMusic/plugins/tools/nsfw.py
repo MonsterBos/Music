ae181d3a5ca73251abea0a3e5d361fb27e2263a9
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-18 13:49:30 +0530

    Create nsfw.py

---
+++ YukkiMusic/plugins/tools/nsfw.py
@@ -0,0 +1,83 @@
+import logging 
+
+from os import remove
+
+from lexica import Client as lexi
+
+from telegraph import upload_file
+
+from pyrogram import filters
+
+from pyrogram.types import Message
+
+from YukkiMusic import app
+
+from YukkiMusic.misc import SUDOERS
+
+from YukkiMusic.utils.error import capture_err
+
+from config import adminlist
+
+
+def check_nsfw(image_url: str) -> dict:
+
+    client = lexi()
+
+    response = client.AntiNsfw(image_url)
+
+    if response['content']['sfw'] == True:
+
+        return False
+
+    else:
+
+        return True
+
+        
+
+@app.on_message(
+
+    ~filters.service
+
+    & ~filters.private
+
+    & ~filters.channel
+
+    & filters.photo,
+
+    group=6
+
+)
+
+@capture_err
+
+async def nsfw(_, message: Message):
+
+	if message.from_user.id in adminlist or message.from_user.id in SUDOERS:
+
+		return 
+
+	photo = await app.download_media(message.photo.file_id)
+  a = upload_file(media)[0]
+  url = "https://telegra.ph" + a
+	try:
+
+    	nsfw = check_nsfw(url)
+
+    	if nsfw == True:
+
+	    	await message.reply_text("Nsfw detected")
+
+    	elif nsfw == False:
+
+	    	await message.reply_text("safe file no nsfw detected")
+
+	    remove(photo)
+
+	except Exception as e:
+
+		remove(photo)
+
+		logging.execption(e)
+
+	
