b79766e32f2e038f3024c53cda65dd2ab632781a
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-19 12:36:08 +0530

    Update Instagram.py

--- YukkiMusic/plugins/tools/Instagram.py
+++ YukkiMusic/plugins/tools/Instagram.py
@@ -4,14 +4,16 @@ import requests
 from YukkiMusic import app
 
 
-@app.on_message(filters.command("im", prefixes="/"))
+@app.on_message(filters.command(["ig","instagram","reel"]))
 async def download_instagram_video(client, message):
     if len(message.command) < 2:
         await message.reply_text(
-            "Please provide the Instagram video URL after the command."
+            "Pʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴛʜᴇ Iɴsᴛᴀɢʀᴀᴍ ʀᴇᴇʟ URL ᴀғᴛᴇʀ ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅ"
         )
         return
-
+    a = await message.reply_text(
+            "ᴘʀᴏᴄᴇssɪɴɢ..."
+        )
     url = message.text.split()[1]
     api_url = (
         f"https://nodejs-1xn1lcfy3-jobians.vercel.app/v2/downloader/instagram?url={url}"
@@ -22,6 +24,13 @@ async def download_instagram_video(client, message):
 
     if data["status"]:
         video_url = data["data"][0]["url"]
+        await a.delete()
         await client.send_video(message.chat.id, video_url)
     else:
-        await message.reply_text("Failed to download video.")
+        await a.edit("Fᴀɪʟᴇᴅ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ʀᴇᴇʟ")
+
+__MODULE__ = "Iɴsᴛᴀɢʀᴀᴍ"
+__HELP__ = """/reel [ɪɴsᴛᴀɢʀᴀᴍ ʀᴇᴇʟ ᴜʀʟ] - Tᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴛʜᴇ ʀᴇᴇʟ ʙʏ ʙᴏᴛ
+/ig [ɪɴsᴛᴀɢʀᴀᴍ ʀᴇᴇʟ ᴜʀʟ] - Tᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴛʜᴇ ʀᴇᴇʟ ʙʏ ʙᴏᴛ
+/instagram [ɪɴsᴛᴀɢʀᴀᴍ ʀᴇᴇʟ ᴜʀʟ] - Tᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴛʜᴇ ʀᴇᴇʟ ʙʏ ʙᴏᴛ
+"""
