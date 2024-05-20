4dbac40df8abf5ae2d1fc493c22c1049037e73b7
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-20 19:05:34 +0530

    Update inlinefunction.py

--- YukkiMusic/utils/inlinefunction.py
+++ YukkiMusic/utils/inlinefunction.py
@@ -2,7 +2,6 @@ from math import ceil
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from YukkiMusic import MOD_LOAD, MOD_NOLOAD
 
-
 class EqInlineKeyboardButton(InlineKeyboardButton):
     def __eq__(self, other):
         return self.text == other.text
@@ -12,7 +11,6 @@ class EqInlineKeyboardButton(InlineKeyboardButton):
 
     def __gt__(self, other):
         return self.text > other.text
-
 
 def paginate_modules(page_n, module_dict, prefix, chat=None):
     if not chat:
@@ -40,16 +38,16 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
             ]
         )
 
-    pairs = [modules[i : i + COLUMN_SIZE] for i in range(0, len(modules), COLUMN_SIZE)]
+    COLUMN_SIZE = 3  # Controls the height of the layout
+    NUM_COLUMNS = 3  # Controls the width of the layout
 
-    COLUMN_SIZE = 3
-    NUM_COLUMNS = 4
+    pairs = [modules[i : i + NUM_COLUMNS] for i in range(0, len(modules), NUM_COLUMNS)]
 
-    max_num_pages = ceil(len(pairs) / NUM_COLUMNS) if len(pairs) > 0 else 1
+    max_num_pages = ceil(len(pairs) / COLUMN_SIZE) if len(pairs) > 0 else 1
     modulo_page = page_n % max_num_pages
 
-    if len(pairs) > NUM_COLUMNS:
-        pairs = pairs[modulo_page * NUM_COLUMNS : NUM_COLUMNS * (modulo_page + 1)] + [
+    if len(pairs) > COLUMN_SIZE:
+        pairs = pairs[modulo_page * COLUMN_SIZE : COLUMN_SIZE * (modulo_page + 1)] + [
             (
                 EqInlineKeyboardButton(
                     "❮",
