1bcb4b2178ff3196c1c16254a3a26d0be666c213
VivekKumar-IN <VivekKumar-IN@users.noreply.github.com>
2024-05-20 05:52:01 +0000

    Auto fixes

--- YukkiMusic/plugins/tools/tts.py
+++ YukkiMusic/plugins/tools/tts.py
@@ -21,6 +21,7 @@ async def text_to_speech(client, message):
     audio_file.name = "audio.mp3"
     await message.reply_audio(audio_file)
 
+
 __MODULE__ = "Tᴛs"
 __HELP__ = """
 /tts [ᴛᴇxᴛ] - ɢᴇɴᴇʀᴀᴛᴇ ᴀᴜᴅɪᴏ ғʀᴏᴍ ᴛᴇxᴛ"""
