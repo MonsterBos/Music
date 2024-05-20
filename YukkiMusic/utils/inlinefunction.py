a78029ffa255b59e5d9692c6afd2fdfbd1878bfa
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-20 09:53:01 +0530

    Update inlinefunction.py

--- YukkiMusic/utils/inlinefunction.py
+++ YukkiMusic/utils/inlinefunction.py
@@ -1,7 +1,5 @@
 from math import ceil
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from YukkiMusic import MOD_LOAD, MOD_NOLOAD
-
+from pyrogram.types import InlineKeyboardButton
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
     def __eq__(self, other):
@@ -13,16 +11,13 @@ class EqInlineKeyboardButton(InlineKeyboardButton):
     def __gt__(self, other):
         return self.text > other.text
 
-
 def paginate_modules(page_n, module_dict, prefix, chat=None):
     if not chat:
         modules = sorted(
             [
                 EqInlineKeyboardButton(
                     x.__MODULE__,
-                    callback_data="{}_module({},{})".format(
-                        prefix, x.__MODULE__.lower(), page_n
-                    ),
+                    callback_data="{}_module({},{})".format(prefix, x.__MODULE__.lower(), page_n),
                 )
                 for x in module_dict.values()
             ]
@@ -40,7 +35,7 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
             ]
         )
 
-    pairs = [modules[i : i + 3] for i in range(0, len(modules), 3)]
+    pairs = [modules[i:i + 3] for i in range(0, len(modules), 3)]
 
     COLUMN_SIZE = 3
 
@@ -48,14 +43,11 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
     modulo_page = page_n % max_num_pages
 
     if len(pairs) > COLUMN_SIZE:
-        pairs = pairs[modulo_page * COLUMN_SIZE : COLUMN_SIZE * (modulo_page + 1)] + [
+        pairs = pairs[modulo_page * COLUMN_SIZE: COLUMN_SIZE * (modulo_page + 1)] + [
             (
                 EqInlineKeyboardButton(
                     "❮",
-                    callback_data="{}_prev({})".format(
-                        prefix,
-                        modulo_page - 1 if modulo_page > 0 else max_num_pages - 1,
-                    ),  # Corrected callback data
+                    callback_data="{}_prev({})".format(prefix, modulo_page - 1 if modulo_page > 0 else max_num_pages - 1),
                 ),
                 EqInlineKeyboardButton(
                     "ᴄʟᴏsᴇ",
