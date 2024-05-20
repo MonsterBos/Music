1e182828a55c8e20dc905afb0bfc8f047546a923
VivekKumar-IN <VivekKumar-IN@users.noreply.github.com>
2024-05-20 02:49:21 +0000

    Auto fixes

--- YukkiMusic/utils/inlinefunction.py
+++ YukkiMusic/utils/inlinefunction.py
@@ -2,6 +2,7 @@ from math import ceil
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from YukkiMusic import MOD_LOAD, MOD_NOLOAD
 
+
 class EqInlineKeyboardButton(InlineKeyboardButton):
     def __eq__(self, other):
         return self.text == other.text
@@ -12,13 +13,16 @@ class EqInlineKeyboardButton(InlineKeyboardButton):
     def __gt__(self, other):
         return self.text > other.text
 
+
 def paginate_modules(page_n, module_dict, prefix, chat=None):
     if not chat:
         modules = sorted(
             [
                 EqInlineKeyboardButton(
                     x.__MODULE__,
-                    callback_data="{}_module({},{})".format(prefix, x.__MODULE__.lower(), page_n),
+                    callback_data="{}_module({},{})".format(
+                        prefix, x.__MODULE__.lower(), page_n
+                    ),
                 )
                 for x in module_dict.values()
             ]
@@ -36,7 +40,7 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
             ]
         )
 
-    pairs = [modules[i:i+3] for i in range(0, len(modules), 3)]
+    pairs = [modules[i : i + 3] for i in range(0, len(modules), 3)]
 
     COLUMN_SIZE = 2
 
@@ -48,7 +52,10 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
             (
                 EqInlineKeyboardButton(
                     "❮",
-                    callback_data="{}_prev({})".format(prefix, modulo_page - 1 if modulo_page > 0 else max_num_pages - 1),  # Corrected callback data
+                    callback_data="{}_prev({})".format(
+                        prefix,
+                        modulo_page - 1 if modulo_page > 0 else max_num_pages - 1,
+                    ),  # Corrected callback data
                 ),
                 EqInlineKeyboardButton(
                     "ᴄʟᴏsᴇ",
