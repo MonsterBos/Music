3a41094197e75b90a6a6f4ab6f872f326af513d4
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-20 09:08:46 +0530

    Update __main__.py

--- YukkiMusic/__main__.py
+++ YukkiMusic/__main__.py
@@ -79,7 +79,7 @@ async def init():
 
 async def help_parser(name, keyboard=None):
     if not keyboard:
-        keyboard = InlineKeyboardMarkup(paginate_modules(0, HELPABLE, "help"))
+        keyboard = InlineKeyboardMarkup(paginate_modules(0, HELPABLE, "help",chat=6815918609))
     return (
         """ʜᴇʟʟᴏ {first_name},
 
