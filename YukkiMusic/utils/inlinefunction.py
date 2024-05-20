33d5575e23b3d4ca4d7aa9759464c096674605fe
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-20 09:56:34 +0530

    Update inlinefunction.py

--- YukkiMusic/utils/inlinefunction.py
+++ YukkiMusic/utils/inlinefunction.py
@@ -1,6 +1,7 @@
+import re
 from math import ceil
-from pyrogram.types import InlineKeyboardButton
-
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
     def __eq__(self, other):
@@ -12,58 +13,28 @@ class EqInlineKeyboardButton(InlineKeyboardButton):
     def __gt__(self, other):
         return self.text > other.text
 
-
 def paginate_modules(page_n, module_dict, prefix, chat=None):
-    if not chat:
-        modules = sorted(
-            [
-                EqInlineKeyboardButton(
-                    x.__MODULE__,
-                    callback_data="{}_module({},{})".format(
-                        prefix, x.__MODULE__.lower(), page_n
-                    ),
-                )
-                for x in module_dict.values()
-            ]
-        )
-    else:
-        modules = sorted(
-            [
-                EqInlineKeyboardButton(
-                    x.__MODULE__,
-                    callback_data="{}_module({},{},{})".format(
-                        prefix, chat, x.__MODULE__.lower(), page_n
-                    ),
-                )
-                for x in module_dict.values()
-            ]
-        )
+    modules = sorted(
+        [
+            EqInlineKeyboardButton(
+                x.__MODULE__,
+                callback_data=f"{prefix}_module({x.__MODULE__.lower()},{page_n})",
+            )
+            for x in module_dict.values()
+        ]
+    )
 
-    pairs = [modules[i : i + 3] for i in range(0, len(modules), 3)]
-
-    COLUMN_SIZE = 3
-
+    pairs = [modules[i:i + 3] for i in range(0, len(modules), 3)]
+    COLUMN_SIZE = 5
     max_num_pages = ceil(len(pairs) / COLUMN_SIZE)
     modulo_page = page_n % max_num_pages
 
     if len(pairs) > COLUMN_SIZE:
-        pairs = pairs[modulo_page * COLUMN_SIZE : COLUMN_SIZE * (modulo_page + 1)] + [
+        pairs = pairs[modulo_page * COLUMN_SIZE:COLUMN_SIZE * (modulo_page + 1)] + [
             (
-                EqInlineKeyboardButton(
-                    "❮",
-                    callback_data="{}_prev({})".format(
-                        prefix,
-                        modulo_page - 1 if modulo_page > 0 else max_num_pages - 1,
-                    ),
-                ),
-                EqInlineKeyboardButton(
-                    "ᴄʟᴏsᴇ",
-                    callback_data="close",
-                ),
-                EqInlineKeyboardButton(
-                    "❯",
-                    callback_data="{}_next({})".format(prefix, modulo_page + 1),
-                ),
+                EqInlineKeyboardButton("❮", callback_data=f"{prefix}_prev({modulo_page})"),
+                EqInlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close"),
+                EqInlineKeyboardButton("❯", callback_data=f"{prefix}_next({modulo_page})"),
             )
         ]
 
