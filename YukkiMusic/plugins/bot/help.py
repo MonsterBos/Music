99b2d7771c62cac161b58c50f66fd822cfd5e28e
VivekKumar-IN <VivekKumar-IN@users.noreply.github.com>
2024-05-20 09:09:25 +0000

    Auto fixes

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -75,7 +75,6 @@ async def helper_private(
             )
 
 
-
 @app.on_message(filters.command(HELP_COMMAND) & filters.group & ~BANNED_USERS)
 @LanguageStart
 async def help_com_group(client, message: Message, _):
