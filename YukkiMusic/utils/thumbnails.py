a5048c3100e63e5f2621dcec6fc7ce9230fed6dc
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-21 13:06:29 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -9,19 +9,27 @@
 
 from YukkiMusic import app
 from config import YOUTUBE_IMG_URL
+from youtubesearchpython.__future__ import VideosSearch
 
 
 async def gen_thumb(videoid):
     try:
-        url = f"https://img.youtube.com/vi/{videoid}/maxresdefault.jpg"
-        return url
-    except Exception:
-        return YOUTUBE_IMG_URL
+        query = f"https://www.youtube.com/watch?v={videoid}"
+        results = VideosSearch(query, limit=1)
+        for result in (await results.next())["result"]:
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        return thumbnail
+    except Exception as e:
+        return config.YOUTUBE_IMG_URL
 
 
-async def gen_qthumb(videoid):
+async def gen_qthumb(vidid):
     try:
-        url = f"https://img.youtube.com/vi/{videoid}/maxresdefault.jpg"
-        return url
-    except Exception:
-        return YOUTUBE_IMG_URL
+        # Search for the video using video ID
+        query = f"https://www.youtube.com/watch?v={vidid}"
+        results = VideosSearch(query, limit=1)
+        for result in (await results.next())["result"]:
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        return thumbnail
+    except Exception as e:
+        return config.YOUTUBE_IMG_URL
