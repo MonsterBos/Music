3e92e7a20001668129688b76bb1839a414a91bb7
VivekKumar-IN <VivekKumar-IN@users.noreply.github.com>
2024-05-21 05:30:11 +0000

    Auto fixes

--- YukkiMusic/plugins/tools/fun.py
+++ YukkiMusic/plugins/tools/fun.py
@@ -3,16 +3,14 @@ from pyrogram.types import Message
 from YukkiMusic import app
 
 
-@app.on_message(filters.command(["dice", "ludo","dart","basket", "basketball"]))
+@app.on_message(filters.command(["dice", "ludo", "dart", "basket", "basketball"]))
 async def dice(c, m: Message):
 
     value = await c.send_dice(m.chat.id, reply_to_message_id=m.id)
     await value.reply_text("results is {0}".format(value.dice.value))
 
-
     value = await c.send_dice(m.chat.id, emoji="🎯", reply_to_message_id=m.id)
     await value.reply_text("results is {0}".format(value.dice.value))
-
 
     basket = await c.send_dice(m.chat.id, emoji="🏀", reply_to_message_id=m.id)
     await basket.reply_text("results is {0}".format(basket.dice.value))
