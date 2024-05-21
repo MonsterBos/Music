7d9695109dfda28c2254a182320eb6291b519782
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-21 10:59:53 +0530

    Update fun.py

--- YukkiMusic/plugins/tools/fun.py
+++ YukkiMusic/plugins/tools/fun.py
@@ -3,19 +3,16 @@ from pyrogram.types import Message
 from YukkiMusic import app
 
 
-@app.on_message(filters.command(["dice", "ludo"]))
+@app.on_message(filters.command(["dice", "ludo","dart","basket", "basketball"]))
 async def dice(c, m: Message):
-    dicen = await c.send_dice(m.chat.id, reply_to_message_id=m.id)
-    await dicen.reply_text("results is {0}".format(dicen.dice.value))
+
+    value = await c.send_dice(m.chat.id, reply_to_message_id=m.id)
+    await value.reply_text("results is {0}".format(value.dice.value))
 
 
-@app.on_message(filters.command(["dart"]))
-async def dice(c, m: Message):
-    dart = await c.send_dice(m.chat.id, emoji="🎯", reply_to_message_id=m.id)
-    await dart.reply_text("results is {0}".format(dart.dice.value))
+    value = await c.send_dice(m.chat.id, emoji="🎯", reply_to_message_id=m.id)
+    await value.reply_text("results is {0}".format(value.dice.value))
 
 
-@app.on_message(filters.command(["basket", "basketball"]))
-async def dice(c, m: Message):
     basket = await c.send_dice(m.chat.id, emoji="🏀", reply_to_message_id=m.id)
     await basket.reply_text("results is {0}".format(basket.dice.value))
