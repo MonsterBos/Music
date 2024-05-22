99e0e1494c4136ce9721f94d08344c41d451092a
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-22 08:07:43 +0530

    Update google.py

--- YukkiMusic/plugins/tools/google.py
+++ YukkiMusic/plugins/tools/google.py
@@ -3,6 +3,29 @@ from pyrogram import filters
 from pyrogram.enums import ChatAction
 from googlesearch import search
 from YukkiMusic import app
+from search_engine_parser.core.engines.bing import Search as BingSearch
+
+bsearch = BingSearch()
+
+@app.on_message(filters.command(["bing"]))
+async def google(bot, message):
+    if len(message.command) < 2 and not message.reply_to_message:
+        await message.reply_text("Example:\n\n`/google lord ram`")
+        return
+
+    if message.reply_to_message and message.reply_to_message.text:
+        user_input = message.reply_to_message.text
+    else:
+        user_input = " ".join(message.command[1:])
+    b = await message.reply_text("**Sᴇᴀʀᴄʜɪɴɢ ᴏɴ Gᴏᴏɢʟᴇ....**")
+    try:
+        search_args = (user_input, 1)
+        results = bsearch.search(*search_args)
+        await b.edit(results)
+    except Exception as e:
+        await b.edit(e)
+
+
 
 
 @app.on_message(filters.command(["google", "gle"]))
