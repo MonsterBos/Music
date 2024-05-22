1959a16759a8f2170bd3f2c2eee287d3e2ee4ef7
VivekKumar-IN <VivekKumar-IN@users.noreply.github.com>
2024-05-22 13:07:03 +0000

    Auto fixes

--- YukkiMusic/plugins/tools/advice.py
+++ YukkiMusic/plugins/tools/advice.py
@@ -23,6 +23,7 @@ async def advice(_, message):
         await message.reply_photo("ᴛʀʏ ᴀғᴛᴇʀ sᴏᴍᴇ ᴛɪᴍᴇ")
         await app.send_message(LOG_GROUP_ID, "/astronomical not working")
 
+
 __MODULE__ = "Cᴏɴᴛᴇɴᴛ's"
 __HELP__ = """
 /astronomical - ᴛᴏ ɢᴇᴛ ᴛᴏᴅᴀʏ's ᴀsᴛʀᴏɴᴏᴍɪᴄᴀʟ ғᴀᴄᴛ"""
