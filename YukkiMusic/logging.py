5cee52d5e33f912f249353129252612c9f5d0ec5
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-22 09:48:40 +0530

    Update logging.py

--- YukkiMusic/logging.py
+++ YukkiMusic/logging.py
@@ -24,10 +24,11 @@ logging.basicConfig(
 )
 
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("telethon").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
+logging.getLogger("pymongo").setLevel(logging.ERROR)
 logging.getLogger("ntgcalls").setLevel(logging.ERROR)
 logging.getLogger("httpx").setLevel(logging.ERROR)
+logging.getLogger("telethon").setLevel(logging.ERROR)
 
 
 def LOGGER(name: str) -> logging.Logger:
