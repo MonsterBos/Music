504de150c645e5c93adf4c6bb9e64a297980726c
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-22 19:06:28 +0530

    Update google.py

--- YukkiMusic/plugins/tools/google.py
+++ YukkiMusic/plugins/tools/google.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from pyrogram.enums import ChatAction
 from googlesearch import search
 from YukkiMusic import app
-
+from YukkiMusic import api
 
 @app.on_message(filters.command(["google", "gle"]))
 async def google(bot, message):
@@ -28,7 +28,31 @@ async def google(bot, message):
     except Exception as e:
         await b.edit(e)
         logging.exception(e)
+@app.on_message(filters.command(["app", "apps"]))
+async def app(bot, message):
+    if len(message.command) < 2 and not message.reply_to_message:
+        await message.reply_text("Example:\n\n`/google lord ram`")
+        return
 
+    if message.reply_to_message and message.reply_to_message.text:
+        user_input = message.reply_to_message.text
+    else:
+        user_input = " ".join(message.command[1:])
+    aku = await message.reply_text("**Sᴇᴀʀᴄʜɪɴɢ ᴏɴ Gᴏᴏɢʟᴇ....**")
+
+a = await api.apps(user_input, 1)
+
+b = a['results'][0]
+icon = b['icon']
+id = b['id']
+link = b['link']
+ca  = b['description']
+title = b['title']
+dev = b['developer']
+info = f"<b>[ᴛɪᴛʟᴇ : {title}]({link})</b>\n<b>ɪᴅ</b>: <code>{id}</code>\n<b>ᴅᴇᴠᴇʟᴏᴘᴇʀ</b> : {dev}\n<b>ᴅᴇsᴄʀɪᴘᴛɪᴏɴ </b>: {ca}"
+await aku.delete()
+await message.reply_photo(icon, caption=info)
 
 __MODULE__ = "Gᴏᴏɢʟᴇ"
-__HELP__ = """/google [ǫᴜᴇʀʏ] - ᴛᴏ sᴇᴀʀᴄʜ ᴏɴ ɢᴏᴏɢʟᴇ ᴀɴᴅ ɢᴇᴛ ʀᴇsᴜʟᴛs"""
+__HELP__ = """/google [ǫᴜᴇʀʏ] - ᴛᴏ sᴇᴀʀᴄʜ ᴏɴ ɢᴏᴏɢʟᴇ ᴀɴᴅ ɢᴇᴛ ʀᴇsᴜʟᴛs
+/app | /apps [ᴀᴘᴘ ɴᴀᴍᴇ] - ᴛᴏ ɢᴇᴛ ᴀᴘᴘ ɪɴғᴏ ᴛʜᴀᴛ ᴀᴠᴀɪʟᴀʙʟᴇ ᴏɴ ᴘʟᴀʏsᴛᴏʀᴇ"""
