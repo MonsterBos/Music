d844e0983ff46ac674e47738f67266b6f45f7153
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-22 13:27:25 +0530

    Update start.py

--- YukkiMusic/plugins/bot/start.py
+++ YukkiMusic/plugins/bot/start.py
@@ -13,7 +13,7 @@ import time
 
 from pyrogram import filters
 from pyrogram.enums import ChatType, ParseMode
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, InputMediaPhoto
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
