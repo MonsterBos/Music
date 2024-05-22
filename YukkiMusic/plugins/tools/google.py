0d361bb875e844f6a035afae94ab35fb91290fdf
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-22 19:12:03 +0530

    Update google.py

--- YukkiMusic/plugins/tools/google.py
+++ YukkiMusic/plugins/tools/google.py
@@ -34,7 +34,7 @@ async def google(bot, message):
 @app.on_message(filters.command(["app", "apps"]))
 async def app(bot, message):
     if len(message.command) < 2 and not message.reply_to_message:
-        await message.reply_text("Example:\n\n`/google lord ram`")
+        await message.reply_text("Example:\n\n`/app Free Fire`")
         return
 
     if message.reply_to_message and message.reply_to_message.text:
