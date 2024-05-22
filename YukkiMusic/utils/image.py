9b90451ce7d740b76dd93978257e78e851f8d285
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-22 13:01:10 +0530

    Update image.py

--- YukkiMusic/utils/image.py
+++ YukkiMusic/utils/image.py
@@ -2,13 +2,14 @@ import requests
 import json
 from config import START_IMG_URL
 
-asyns def gen_image():
+def gen_image():
     try:
         url = "https://random.imagecdn.app/v1/image?width=1280&height=720&format=json"
 
         response = requests.get(url)
         data = response.json()
-        return data.get("url")
+        Z = data.get("url")
+        return Z
     except Exception:
         return START_IMG_URL
-
+IMAGE = gen_image()
