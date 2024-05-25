11c1765f09f1af323438f4682cc20a74caca9c47
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-25 21:52:45 +0530

    Update tools

---
+++ YukkiMusic/plugins/tools/atag.py
@@ -0,0 +1,114 @@
+import asyncio
+import random
+from pyrogram import Client, filters
+from pyrogram.enums import ChatType, ChatMemberStatus, ChatMembersFilter
+from pyrogram.errors import UserNotParticipant, FloodWait
+from pyrogram.types import ChatPermissions
+from YukkiMusic import app
+from YukkiMusic.utils.filter import admin_filter
+from YukkiMusic.utils.database import get_assistant
+
+SPAM_CHATS = []
+
+
+@app.on_message(
+    filters.command(
+        [
+            "astopmention",
+            "acancel",
+            "acancelmention",
+            "aoffmention",
+            "amentionoff",
+            "acancelall",
+        ],
+        prefixes=["/", "@", "#"],
+    )
+    & admin_filter
+)
+async def cancelcmd(_, message):
+    chat_id = message.chat.id
+    if chat_id in SPAM_CHATS:
+        try:
+            SPAM_CHATS.remove(chat_id)
+        except Exception:
+            pass
+        return await message.reply_text("**ᴛᴀɢɢɪɴɢ ᴘʀᴏᴄᴇss sᴜᴄᴄᴇssғᴜʟʟʏ sᴛᴏᴘᴘᴇᴅ!**")
+
+    else:
+        await message.reply_text("**ɴᴏ ᴘʀᴏᴄᴇss ᴏɴɢᴏɪɴɢ!**")
+        return
+
+
+
+
+
+@app.on_message(
+    filters.command(["aall", "amention", "amentionall", "atagall"], prefixes=["/", "@"])
+    & admin_filter
+)
+async def tag_all_users(_, message):
+    if message.chat.id in SPAM_CHATS:
+        return await message.reply_text(
+            "ᴛᴀɢɢɪɴɢ ᴘʀᴏᴄᴇss ɪs ᴀʟʀᴇᴀᴅʏ ʀᴜɴɴɪɴɢ ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ sᴛᴏᴘ sᴏ ᴜsᴇ /cancel"
+        )
+    userbot = get_assistant(message.chat.id)
+    replied = message.reply_to_message
+    if len(message.command) < 2 and not replied:
+        await message.reply_text(
+            "** ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴛᴀɢ ᴀʟʟ, ʟɪᴋᴇ »** `@aall Hi Friends`"
+        )
+        return
+    if replied:
+        try:
+             SPAM_CHATS.append(message.chat.id)
+            usernum = 0
+            usertxt = ""
+            async for m in app.get_chat_members(message.chat.id):
+                if message.chat.id not in SPAM_CHATS:
+                    break
+                usernum += 1
+                usertxt += f"[{m.user.first_name}](tg://user?id={m.user.id})"
+                if usernum == 7:
+                    ftext = f"{message.reply_to_message.text}\n{usertxt}"
+                    await userbot.send_message(
+                        chat_id=message.chat.id,
+                        text=ftext,
+                        disable_web_page_preview=True,
+                    )
+                    await asyncio.sleep(1)
+                    usernum = 0
+                    usertxt = ""
+
+        except FloodWait as e:
+            await asyncio.sleep(e.value)
+        try:
+            SPAM_CHATS.remove(message.chat.id)
+        except Exception:
+            pass
+    else:
+        try:
+            userbot = get_assistant(message.chat.id)
+            text = message.text.split(None, 1)[1]
+            SPAM_CHATS.append(message.chat.id)
+            usernum = 0
+            usertxt = ""
+            async for m in app.get_chat_members(message.chat.id):
+                if message.chat.id not in SPAM_CHATS:
+                    break
+                usernum += 1
+                usertxt += f"[{m.user.first_name}](tg://user?id={m.user.id})"
+                if usernum == 7:
+                    await userbot.send_message(
+                        message.chat.id,
+                        f"{text}\n{usertxt}",
+                        disable_web_page_preview=True,
+                    )
+                    await asyncio.sleep(2)
+                    usernum = 0
+                    usertxt = ""
+        except FloodWait as e:
+            await asyncio.sleep(e.value)
+        try:
+            SPAM_CHATS.remove(message.chat.id)
+        except Exception:
+            pass
