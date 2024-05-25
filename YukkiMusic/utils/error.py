e63d3698916056f99a785b01aea5755161ccdcfb
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-25 14:15:02 +0530

    Update error.py

--- YukkiMusic/utils/error.py
+++ YukkiMusic/utils/error.py
@@ -35,7 +35,7 @@ def capture_err(func):
         except Exception as err:
             errors = traceback.format_exc()
             error_feedback = split_limits(
-                "**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n".format(
+                "**ERROR** | `{}` | `{}`\n```python\n{}```\n\n```python\n{}```\n".format(
                     0 if not message.from_user else message.from_user.id,
                     0 if not message.chat else message.chat.id,
                     message.text or message.caption,
