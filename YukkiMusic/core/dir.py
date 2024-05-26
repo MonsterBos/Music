3ec21484964b11c620edf974c4c147d684be7b52
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-26 11:43:18 +0530

    Update dir.py

--- YukkiMusic/core/dir.py
+++ YukkiMusic/core/dir.py
@@ -18,6 +18,7 @@ def dirr():
     assets_folder = "assets"
     downloads_folder = "downloads"
     cache_folder = "cache"
+    workdir = "datafiles"
 
     if assets_folder not in listdir():
         logging.warning(
@@ -34,6 +35,9 @@ def dirr():
 
     if cache_folder not in listdir():
         mkdir(cache_folder)
+
+    if workdir not in listdir():
+        mkdir(workdir)
 
     logging.info("Directories Updated.")
 
