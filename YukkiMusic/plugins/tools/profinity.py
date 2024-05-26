3a9763eab4b41a921985ca34e8b829f5572ac014
VivekKumar-IN <VivekKumar-IN@users.noreply.github.com>
2024-05-26 08:19:57 +0000

    Auto fixes

--- YukkiMusic/plugins/tools/profinity.py
+++ YukkiMusic/plugins/tools/profinity.py
@@ -10,6 +10,7 @@ from YukkiMusic.misc import SUDOERS
 from profanity import profanity
 from YukkiMusic.utils.error import capture_err
 
+
 @app.on_message(filters.text & filters.group, group=11)
 @capture_err
 async def handle_bad_words(client, message):
@@ -21,15 +22,17 @@ async def handle_bad_words(client, message):
 
         censored_text = profanity.censor(txt)
         bot_member = await app.get_chat_member(message.chat.id, app.id)
-        
+
         if bot_member is None:
             logging.error("Bot member status could not be retrieved.")
             return
-        
+
         bot = bot_member.privileges
         chat_id = message.chat.id
         admins = []
-        async for admin in app.get_chat_members(message.chat.id, filter=ChatMembersFilter.ADMINISTRATORS):
+        async for admin in app.get_chat_members(
+            message.chat.id, filter=ChatMembersFilter.ADMINISTRATORS
+        ):
             admins.append(admin.user)
 
         if user_id in SUDOERS or user_id in [admin.id for admin in admins]:
@@ -38,7 +41,7 @@ async def handle_bad_words(client, message):
         if bot is None:
             logging.error("Bot privileges could not be determined.")
             return
-        
+
         for admin in admins:
             if admin.is_bot or admin.is_deleted:
                 continue
@@ -51,14 +54,18 @@ async def handle_bad_words(client, message):
             )
         elif bot.can_restrict_members and not bot.can_delete_messages:
             mute_time = datetime.datetime.now() + datetime.timedelta(minutes=5)
-            await app.restrict_chat_member(chat_id, user_id, ChatPermissions(), until_date=mute_time)
+            await app.restrict_chat_member(
+                chat_id, user_id, ChatPermissions(), until_date=mute_time
+            )
             await message.reply_text(
                 f"{message.from_user.mention} used a bad word: **{censored_text}**, so they are muted for 5 minutes but I have no permission to delete messages so give me delete message permission in order to delete bad words automatically."
             )
         elif bot.can_restrict_members and bot.can_delete_messages:
             await message.delete()
             mute_time = datetime.datetime.now() + datetime.timedelta(minutes=5)
-            await app.restrict_chat_member(chat_id, user_id, ChatPermissions(), until_date=mute_time)
+            await app.restrict_chat_member(
+                chat_id, user_id, ChatPermissions(), until_date=mute_time
+            )
             SH = await app.send_message(
                 message.chat.id,
                 f"{message.from_user.mention} used a bad word: **{censored_text}**, so they are muted for 5 minutes.",
