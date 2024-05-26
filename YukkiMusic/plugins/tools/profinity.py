6cc34ce6d35cdb377ad74d3d2ac31dd9c007d6ed
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-26 13:53:35 +0530

    Update profinity.py

--- YukkiMusic/plugins/tools/profinity.py
+++ YukkiMusic/plugins/tools/profinity.py
@@ -19,15 +19,8 @@ async def handle_bad_words(client, message):
         user_id = message.from_user.id
         if not profanity.contains_profanity(txt):
             return
-
         censored_text = profanity.censor(txt)
-        bot_member = await app.get_chat_member(message.chat.id, app.id)
-
-        if bot_member is None:
-            logging.error("Bot member status could not be retrieved.")
-            return
-
-        bot = bot_member.privileges
+        bot = (await app.get_chat_member(message.chat.id, app.id)).privileges
         chat_id = message.chat.id
         admins = []
         async for admin in app.get_chat_members(
@@ -35,13 +28,12 @@ async def handle_bad_words(client, message):
         ):
             admins.append(admin.user)
 
-        if user_id in SUDOERS or user_id in [admin.id for admin in admins]:
+        if message.from_user.id in SUDOERS or message.from_user.id in [
+            admin.id for admin in admins
+        ]:
             return
-
-        if bot is None:
-            logging.error("Bot privileges could not be determined.")
+        if bot == None:
             return
-
         for admin in admins:
             if admin.is_bot or admin.is_deleted:
                 continue
@@ -50,7 +42,7 @@ async def handle_bad_words(client, message):
         if bot.can_delete_messages and not bot.can_restrict_members:
             await message.delete()
             await message.reply_text(
-                f"User {message.from_user.mention} has sent **{censored_text}**. I have deleted the bad word but please give me permission to restrict members in order to automatically mute users who send bad words for 5 minutes."
+                f"User {message.from_user.mention} has sent **{censored_text}**. i have deleted the bad word but Please give me permission to restrict members in order to automatically mute users who send bad words for 5 minutes."
             )
         elif bot.can_restrict_members and not bot.can_delete_messages:
             mute_time = datetime.datetime.now() + datetime.timedelta(minutes=5)
@@ -58,7 +50,7 @@ async def handle_bad_words(client, message):
                 chat_id, user_id, ChatPermissions(), until_date=mute_time
             )
             await message.reply_text(
-                f"{message.from_user.mention} used a bad word: **{censored_text}**, so they are muted for 5 minutes but I have no permission to delete messages so give me delete message permission in order to delete bad words automatically."
+                f"{message.from_user.mention} used a bad word: **{censored_text}**, so they are muted for 5 minutes but i have no permission to delete message so give me delete message permission in order to delete bad word automatically",
             )
         elif bot.can_restrict_members and bot.can_delete_messages:
             await message.delete()
@@ -68,14 +60,15 @@ async def handle_bad_words(client, message):
             )
             SH = await app.send_message(
                 message.chat.id,
-                f"{message.from_user.mention} used a bad word: **{censored_text}**, so they are muted for 5 minutes.",
+                f"{message.from_user.mention} used a bad word: **{censored_text}**, so they are muted for 5 minutes",
             )
             await asyncio.sleep(300)
             await SH.delete()
         elif not bot.can_restrict_members and not bot.can_delete_messages:
             await message.reply_text(
-                f"User {message.from_user.mention} has sent **{censored_text}**. The bad word but please give me permission to restrict members in order to automatically mute users who send bad words for 5 minutes and delete message permission to delete bad messages automatically."
+                f"User {message.from_user.mention} has sent **{censored_text}**. the bad word but Please give me permission to restrict members in order to automatically mute users who send bad words for 5 minutes. and delete message permission to delete bad message automatically"
             )
+
     except Exception as e:
         logging.exception(e)
         await app.send_message(LOG_GROUP_ID, f"Error in profanity module: {e}")
