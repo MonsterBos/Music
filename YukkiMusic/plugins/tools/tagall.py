af317e328a1ddeb0108d50c3a687a140c9e6f78a
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-26 10:15:55 +0530

    Update tagall.py

--- YukkiMusic/plugins/tools/tagall.py
+++ YukkiMusic/plugins/tools/tagall.py
@@ -148,6 +148,71 @@ async def tag_all_users(_, message):
         except Exception:
             pass
 
+@app.on_message(
+    filters.command(
+        ["atag", "aall", "amention", "amentionall"], prefixes=["/", "@", ".", "#"]
+    )
+    & admin_filter
+)
+async def tag_all_useres(_, message):
+    userbot = await get_assistant(message.chat.id)
+    if message.chat.id in SPAM_CHATS:
+        return await message.reply_text(
+            "ᴛᴀɢɢɪɴɢ ᴘʀᴏᴄᴇss ɪs ᴀʟʀᴇᴀᴅʏ ʀᴜɴɴɪɴɢ ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ sᴛᴏᴘ sᴏ ᴜsᴇ /acancel"
+        )
+    replied = message.reply_to_message
+    if len(message.command) < 2 and not replied:
+        await message.reply_text(
+            "** ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴛᴀɢ ᴀʟʟ, ʟɪᴋᴇ »** `@aall Hi Friends`"
+        )
+        return
+    if replied:
+        SPAM_CHATS.append(message.chat.id)
+        usernum = 0
+        usertxt = ""
+        async for m in app.get_chat_members(message.chat.id):
+            if message.chat.id not in SPAM_CHATS:
+                break
+            usernum += 1
+            usertxt += (
+                f"[{m.user.first_name}](tg://openmessage?user_id={m.user.id})"
+            )
+            if usernum == 7:
+                await replied.reply_text(usertxt)
+                await asyncio.sleep(2)
+                usernum = 0
+                usertxt = ""
+        try:
+            SPAM_CHATS.remove(message.chat.id)
+        except Exception:
+            pass
+    else:
+        text = message.text.split(None, 1)[1]
+
+        SPAM_CHATS.append(message.chat.id)
+        usernum = 0
+        usertxt = ""
+        async for m in app.get_chat_members(message.chat.id):
+            if message.chat.id not in SPAM_CHATS:
+                break
+            usernum += 1
+            usertxt += f'<a href="tg://openmessage?user_id={m.user.id}">{m.user.first_name}</a>'
+
+            if usernum == 7:
+                await userbot.send_message(
+                    message.chat.id,
+                    f"{text}\n{usertxt}",
+                )
+                await asyncio.sleep(2)
+                usernum = 0
+                usertxt = ""
+        try:
+            SPAM_CHATS.remove(message.chat.id)
+        except Exception:
+            pass
+
+
+
 
 @app.on_message(
     filters.command(
