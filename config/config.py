d0f86e785f057d3907bcbe7f80cb3c1f19f78b9d
MonsterBos <122009166+MonsterBos@users.noreply.github.com>
2024-05-26 22:53:18 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -89,7 +89,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Vivekkumar-IN/YukkiMusic",
+    "https://github.com/MonsterBos/Music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "dev")
 
